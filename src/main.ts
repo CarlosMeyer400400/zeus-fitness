73f5b661e66796dc6038cd835971ea18ebf85fc1
CARLOSMEYER <20210696@uthh.edu.mx>
2024-12-30 22:45:54 -0600

    bmw 300

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http'; // Importa aquí
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(), // Configura HttpClient
+  ],
+}).catch((err) => console.error(err));
